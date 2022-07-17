609479827193dd8e7ba14394e67b5f22f5f35bd6
vmen0828 <mingzoo2008@gmail.com>
2022-07-17 14:34:59 +0800

    for test

---
+++ ModelBindingDemo/ModelBindingDemo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ModelBindingDemo.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelBindingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(DemoViewModel viewModel)
+        {
+            //這裡接到值
+            return Json(viewModel);
+        }
+    }
+}
---
+++ ModelBindingDemo/ModelBindingDemo/Models/DemoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelBindingDemo.Models
+{
+    public class DemoViewModel
+    {
+        public string test1 { get; set; }
+
+        public List<string> test2 { get; set; }
+
+    }
+}
